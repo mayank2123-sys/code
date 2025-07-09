88ad7436b4aae162a6c5c112f85c2e9f153bc21a
mayank2123-sys <mahajanmayank866@gmail.com>
2025-07-09 17:38:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import {Routes,Route } from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import Login from "./pages/Login.js"
+import Register from './pages/Register';
+
+function App() {
+  return (
+   <>
+   <Routes>
+   <Route path='/' element ={<HomePage />} />
+    <Route path='/register' element ={<Register/>} />
+    <Route path='/login' element ={<Login/>} />
+   </Routes>
+   </>
+  
+  );
+}
+
+export default App;
